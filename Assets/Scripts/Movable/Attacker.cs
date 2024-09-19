a89e60e05e4a4539f664a600015a88c10c723f9c
GalaKtiKos2004 <94827294+GalaKtiKos2004@users.noreply.github.com>
2024-09-19 22:35:42 +1000

    доделан Аттакер

--- Assets/Scripts/Movable/Attacker.cs
+++ Assets/Scripts/Movable/Attacker.cs
@@ -2,21 +2,26 @@ using UnityEngine;
 
 public class Attacker : MonoBehaviour
 {
-    [SerializeField] private LayerMask _attakcedLayer;
+    [SerializeField] private LayerMask _attackedLayer;
     [SerializeField] private float _damage;
+
+    private readonly LayerMask _playerLayer = LayerMask.NameToLayer("Player");
+    private readonly LayerMask _enemyLayer = LayerMask.NameToLayer("Enemy");
 
     private PlayerInput _input;
     private ColliderDetector _detector;
+    private LayerMask _currentLayer;
 
     private void Awake()
     {
         _input = GetComponent<PlayerInput>();
         _detector = GetComponent<ColliderDetector>();
+        _currentLayer = GetComponent<LayerMask>();
     }
 
     private void Update()
     {
-        if (_input.IsAttack)
+        if ((_currentLayer == _playerLayer && _input.IsAttack) || (_currentLayer == _enemyLayer))
         {
             TryAttack();
         }
@@ -24,7 +29,7 @@ public class Attacker : MonoBehaviour
 
     private void TryAttack()
     {
-        if (_detector.IsGrounded(transform, _attakcedLayer, out Collider2D attackedCollider))
+        if (_detector.IsGrounded(transform, _attackedLayer, out Collider2D attackedCollider))
         {
             if (attackedCollider.TryGetComponent(out Health attackedHealth))
             {
